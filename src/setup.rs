466f7083da41f4c3b9a32fbf809aa657b6a6a95d
Austin Riba <austin@m51.io>
2025-06-17 22:36:10 -0700

    More quest setup. Data directory

--- src/setup.rs
+++ src/setup.rs
@@ -7,6 +7,12 @@ use std::path::Path;
 
 pub fn setup(repo_path: &str) -> Result<()> {
     println!("Setting up repository at {}", repo_path);
+    create_post_commit_hook(repo_path)?;
+    create_data_directory(repo_path)?;
+    Ok(())
+}
+
+fn create_post_commit_hook(repo_path: &str) -> Result<()> {
     let post_commit = format!("{}/.git/hooks/post-commit", repo_path);
     let post_commit_path = Path::new(&post_commit);
 
@@ -19,6 +25,17 @@ pub fn setup(repo_path: &str) -> Result<()> {
         let mut file = File::create(post_commit_path).context("Error creating post-commit hook")?;
         file.write_all(hook_content.as_bytes())?;
         fs::set_permissions(post_commit_path, Permissions::from_mode(0o755))?;
+    }
+    Ok(())
+}
+
+fn create_data_directory(repo_path: &str) -> Result<()> {
+    let data_dir = format!("{}/.git/quest/", repo_path);
+    let data_dir_path = Path::new(&data_dir);
+
+    if !data_dir_path.exists() {
+        println!("Creating data directory in {}", data_dir);
+        fs::create_dir_all(data_dir_path)?;
     }
     Ok(())
 }
